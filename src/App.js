2029ed1dd835b921d5e3bec1d53eddb59eb79922
andreaweb <deiainluv@gmail.com>
2018-09-10 18:18:43 -0300

    starting html

--- src/App.js
+++ src/App.js
@@ -7,12 +7,37 @@ class App extends Component {
     return (
       <div className="App">
         <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h1 className="App-title">Welcome to React</h1>
+          <h1 className="App-title">Read.It</h1>
         </header>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
+        <main>
+          <aside>
+            <button>New Post</button>
+            <ul>
+              <li>Fic</li>
+              <li>Non-Fic</li>
+            </ul>
+          </aside>
+          <section>
+            <h2>Title of this post is blah</h2>
+            <div className="votes">
+              12
+            </div>
+            <div className="postSummary">
+              <p>I actually agree with everything</p>
+              <span>John Doe, 20/08/2018</span>
+            </div>
+          </section>
+          <section>
+            <h2>Halb si tsop siht fo eltit</h2>
+            <div className="votes">
+              12
+            </div>
+            <div className="postSummary">
+              <p>I disagree but not really</p>
+              <span>Eod Nhoj, 23/08/2018</span>
+            </div>
+          </section>
+        </main>
       </div>
     );
   }
