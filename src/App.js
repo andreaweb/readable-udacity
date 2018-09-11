fc6cb699fd0ae98fe9287888ca36a848e108043d
andreaweb <deiainluv@gmail.com>
2018-09-11 08:55:44 -0300

    starting CSS

--- src/App.js
+++ src/App.js
@@ -10,14 +10,14 @@ class App extends Component {
           <h1 className="App-title">Read.It</h1>
         </header>
         <main>
-          <aside>
+          <aside className="aside">
             <button>New Post</button>
             <ul>
               <li>Fic</li>
               <li>Non-Fic</li>
             </ul>
           </aside>
-          <section>
+          <section className="post">
             <h2>Title of this post is blah</h2>
             <div className="votes">
               12
@@ -27,7 +27,17 @@ class App extends Component {
               <span>John Doe, 20/08/2018</span>
             </div>
           </section>
-          <section>
+          <section className="post">
+            <h2>Halb si tsop siht fo eltit</h2>
+            <div className="votes">
+              12
+            </div>
+            <div className="postSummary">
+              <p>I disagree but not really</p>
+              <span>Eod Nhoj, 23/08/2018</span>
+            </div>
+          </section>
+          <section className="post">
             <h2>Halb si tsop siht fo eltit</h2>
             <div className="votes">
               12
