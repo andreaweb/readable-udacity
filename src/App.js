64c01e93b0aa943a8dfd77c890fab1662714715f
andreaweb <deiainluv@gmail.com>
2018-09-11 09:39:51 -0300

    using CSS grid for post styles

--- src/App.js
+++ src/App.js
@@ -7,43 +7,65 @@ class App extends Component {
     return (
       <div className="App">
         <header className="App-header">
-          <h1 className="App-title">Read.It</h1>
+          <h1 className="App-title">
+            Read.It
+            <span className="description">
+              A project for Udacity's React Nanodegree
+            </span>
+          </h1>
         </header>
         <main>
           <aside className="aside">
-            <button>New Post</button>
-            <ul>
+            <button className="button button--new">
+              New Post
+            </button>
+            <div className="order-by">
+              <label>Order By</label>
+              <select>
+                <option>Date</option>
+                <option>Votes</option>
+              </select>
+            </div>
+            <ul className="categories">
+              <li className="categories__title">Categories</li>
               <li>Fic</li>
               <li>Non-Fic</li>
             </ul>
           </aside>
+
           <section className="post">
-            <h2>Title of this post is blah</h2>
-            <div className="votes">
-              12
+            <h2 className="post__name">Title of this post is blah</h2>
+            <div className="post__votes">
+              <i className="fa fa-caret-up" />
+              <p>12</p>
+              <i className="fa fa-caret-down" />
             </div>
-            <div className="postSummary">
+            <div className="post__summary">
               <p>I actually agree with everything</p>
               <span>John Doe, 20/08/2018</span>
             </div>
           </section>
           <section className="post">
-            <h2>Halb si tsop siht fo eltit</h2>
-            <div className="votes">
-              12
+            <h2 className="post__name">Halb si tsop siht fo eltit</h2>
+            <div className="post__votes">
+              <i className="fa fa-caret-up" />
+              <p>12</p>
+              <i className="fa fa-caret-down" />
             </div>
-            <div className="postSummary">
+            <div className="post__summary">
               <p>I disagree but not really</p>
               <span>Eod Nhoj, 23/08/2018</span>
             </div>
           </section>
           <section className="post">
-            <h2>Halb si tsop siht fo eltit</h2>
-            <div className="votes">
-              12
+            <h2 className="post__name">Halb si tsop siht fo eltit</h2>
+            <div className="post__votes">
+              <i className="fa fa-caret-up" />
+              <p>12</p>
+              <i className="fa fa-caret-down" />
             </div>
-            <div className="postSummary">
-              <p>I disagree but not really</p>
+            <div className="post__summary">
+              <p>I disagree but not really oaksmalks</p>
               <span>Eod Nhoj, 23/08/2018</span>
             </div>
           </section>
