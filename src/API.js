144e9d88b8a6b865a86ca9868794cc662eaeb6d8
andreaweb <deiainluv@gmail.com>
2018-10-31 16:57:50 -0300

    wrote CreatePost, getPosts and getCategories

--- src/API.js
+++ src/API.js
@@ -1,4 +1,4 @@
-const api = ${process.env.REACT_APP_API_URL}
+const api = process.env.REACT_APP_API_URL
 
 let token = localStorage.token
 if (!token)
@@ -6,7 +6,8 @@ if (!token)
 
 const headers = {
   'Accept': 'application/json',
-  'Authorization': token
+  'Authorization': token,
+  'Content-Type': 'application/json'
 }
 
 export const getCategories = () =>
@@ -14,7 +15,27 @@ export const getCategories = () =>
     .then(res => res.json())
     .then(data => data.categories)
 
+export const getPosts = () =>
+  fetch(`${api}/posts`, { headers })
+    .then(res => res.json())
+    .then(data => data)
+
 export const getPostsInCategory = (category) =>
   fetch(`${api}/${category}/posts`, { headers })
     .then(res => res.json())
-    .then(data => data.posts)
+    .then(data => data)
+
+export const createPost = (post) =>
+  fetch(`${api}/posts/`, { 
+      method: 'POST', 
+      headers: headers, 
+      body: JSON.stringify({
+        title: post, 
+        id: Math.random().toString(36).substr(-8), 
+        timestamp: Date.now(),
+        category: 'redux', 
+        author: "Gloryhammer"
+      })
+    }
+  )
+  .then(res => res.json())
--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,5 +1,6 @@
 import React, { Component } from 'react';
 import {Link} from 'react-router-dom';
+import * as API from '../API.js'
 import Aside from '../components/Aside';
 import Header from '../components/Header';
 import Post from '../components/Post';
@@ -8,26 +9,9 @@ import '../App.css';
 
 class Home extends Component {
   componentDidMount(){
-    fetch(
-        `${process.env.REACT_APP_API_URL}/categories`,
-        {
-            headers: { 
-              'Accept': 'application/json',
-              'Authorization': Math.random().toString(36).substr(-8) 
-            }
-        }
-    ).then(
-      (res) => {
-        console.log(res);
-        return res.json()
-      }
-    ).then(
-      (data) => {
-        console.log(data);
-        return data;
-      }
-    )
-    .catch((error) => console.log(error))
+    console.log(API.getCategories())
+    console.log(API.getPostsInCategory('redux'))
+    console.log(API.getPosts())
   }
   render() {
     return (
